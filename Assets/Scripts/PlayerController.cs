c03185b6666041b7fa29f845f3d80f9c8834f821
Don Chacko <donisepic30@gmail.com>
2020-08-12 18:39:41 +0300

    Implemented Movement and Camera

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 10;
+    public Rigidbody2D rb;
+
+    private Vector2 moveVelocity;
+
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 moveDirection = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        moveVelocity = moveDirection.normalized * speed;
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
+    }
+}
