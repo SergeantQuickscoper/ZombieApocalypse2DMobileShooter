1ef7096d48f9d98ae6fdd51253075148ee5d4277
Don Chacko <donisepic30@gmail.com>
2020-09-02 16:12:25 +0300

    Enemy Particle Effects

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     public float moveSpeed = 230;
+    public int BulletDamage = 10;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,5 +17,17 @@ public class Bullet : MonoBehaviour
     {
         transform.Translate(Vector3.right * Time.deltaTime * moveSpeed);
         Destroy(gameObject, 0.5f);
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        Destroy(this.gameObject);
+        if(collision.CompareTag("Enemy"))
+        {
+            Enemy HitEnemy = collision.gameObject.GetComponent<Enemy>();
+            HitEnemy.DamageEnemy(BulletDamage);
+            Instantiate(HitEnemy.EnemyHitParicles, transform.position, Quaternion.identity);
+
+        }
     }
 }
---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    
+    [SerializeField]
+    public class EnemyStats
+    {
+        public int MaxEnemyHealth = 100; 
+        private int EnemyHealth = 100;
+          public int GetCurrentHealth
+        {
+            get { return EnemyHealth; }
+            set { EnemyHealth = Mathf.Clamp(value, 0, MaxEnemyHealth); }
+        }
+
+    }
+
+     EnemyStats EnemyStatsInstance = new EnemyStats();
+     public Transform EnemyHitParicles;
+     public Transform EnemyDeathParticles;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void DamageEnemy(int Damage)
+    {
+        EnemyStatsInstance.GetCurrentHealth = EnemyStatsInstance.GetCurrentHealth - Damage;
+        if(EnemyStatsInstance.GetCurrentHealth <= 0)
+        {
+            GameMaster.KillEnemy(this);
+        }
+
+
+    }
+}
