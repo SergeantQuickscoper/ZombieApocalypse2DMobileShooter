ec7e0a444782b527cda90043696761205df638ec
Don Chacko <donisepic30@gmail.com>
2020-12-23 14:30:29 +0300

    Finished Basic Anims and House Interior

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,10 +6,8 @@ public class PlayerController : MonoBehaviour
 {
     public float speed = 10;
     public Rigidbody2D rb;
-
+    public Animator anim;
     private Vector2 moveVelocity;
-
-
     
     // Start is called before the first frame update
     void Start()
@@ -22,6 +20,14 @@ public class PlayerController : MonoBehaviour
     {
         Vector2 moveDirection = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         moveVelocity = moveDirection.normalized * speed;
+        if(moveVelocity.x != 0 || moveVelocity.y !=0)
+        {
+             anim.SetBool("IsMoving", true);
+        }
+        else
+        {
+            anim.SetBool("IsMoving", false);
+        }
     }
 
     void FixedUpdate()
