8e43bd03870fb99bd294ba7cbe16ee80302954e3
Don Chacko <donisepic30@gmail.com>
2021-01-09 17:57:44 +0300

    Made SceneManager

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class SceneLoader : MonoBehaviour
+{
+    public string RequiredScene;
+    
+    void OnTriggerStay2D(Collider2D col)
+    {
+        if(col.gameObject.CompareTag("Player"))
+        {
+            if(Input.GetKeyDown(KeyCode.E))
+            {
+                SceneManager.LoadScene(RequiredScene.ToString());
+            }
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
